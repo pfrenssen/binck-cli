c76f76119ddf5a2c7310845bdab0fe675ddf0390
Pieter Frenssen <pieter@frenssen.be>
2017-03-18 01:25:11 +0100

    Export the transactions in xlsx format.

--- src/Command/Export/TransactionHistory.php
+++ src/Command/Export/TransactionHistory.php
@@ -56,6 +56,27 @@ class TransactionHistory extends CommandBase
             $funds[$fund_info['fondsId']] = $fund_info['fondsNaam'];
         }
 
+        // Initialize an Excel sheet to save the data in.
+        $sheet = new \PHPExcel();
+        $sheet->setActiveSheetIndex(0);
+        $sheet_row = 1;
+
+        for ($i = 0; $i < 7; $i++) $sheet->getActiveSheet()->getColumnDimensionByColumn($i)->setAutoSize(TRUE);
+
+        $headers = [
+            'Fund name',
+            'Domicile',
+            'Transaction date',
+            'Transaction type',
+            'Number',
+            'Total',
+            'Share price',
+        ];
+        foreach ($headers as $column => $header) {
+            $sheet->getActiveSheet()->setCellValueByColumnAndRow($column, $sheet_row, $header);
+        }
+        $sheet_row++;
+
         // Visit the transaction history page of each fund and retrieve the
         // data.
         foreach ($funds as $id => $name) {
@@ -86,7 +107,51 @@ class TransactionHistory extends CommandBase
                 ->setHeaders(['Date', 'Transaction', 'Number', 'Position', 'Share price'])
                 ->setRows($transactions);
             $table->render();
+
+            foreach ($transactions as $transaction) {
+                // Effect name.
+                $sheet->getActiveSheet()->setCellValueByColumnAndRow(0, $sheet_row, $name);
+
+                // Domicile.
+                $sheet->getActiveSheet()->setCellValueByColumnAndRow(1, $sheet_row, 'Ireland');
+
+                // Date of transaction.
+                $date = array_shift($transaction);
+                $date = \DateTime::createFromFormat('d/m/Y', $date);
+                $sheet->getActiveSheet()->setCellValueByColumnAndRow(2, $sheet_row, \PHPExcel_Shared_Date::PHPToExcel($date));
+                $sheet->getActiveSheet()->getStyleByColumnAndRow(2, $sheet_row)->getNumberFormat()->setFormatCode('dd.mm.yyyy');
+
+                // Type of transaction.
+                $type = array_shift($transaction);
+                $sheet->getActiveSheet()->setCellValueByColumnAndRow(3, $sheet_row, $type);
+
+                // Quantity of shares.
+                $quantity = strtr(array_shift($transaction), ['.' => '']);
+                $sheet->getActiveSheet()->setCellValueByColumnAndRow(4, $sheet_row, $quantity);
+                if ($quantity < 0) $sheet->getActiveSheet()->getStyleByColumnAndRow(4, $sheet_row)->getFont()->setColor(new \PHPExcel_Style_Color(\PHPExcel_Style_Color::COLOR_RED));
+
+                // Total position.
+                $position = strtr(array_shift($transaction), ['.' => '']);
+                $sheet->getActiveSheet()->setCellValueByColumnAndRow(5, $sheet_row, $position);
+
+                // Share price.
+                $price = strtr(array_shift($transaction), [',' => '.']);
+                $currency = \PHPExcel_Style_NumberFormat::FORMAT_CURRENCY_EUR_SIMPLE;
+                if (substr($price, 0, 1) === '$') {
+                    $price = substr($price, 1);
+                    $currency = \PHPExcel_Style_NumberFormat::FORMAT_CURRENCY_USD_SIMPLE;
+                }
+                $sheet->getActiveSheet()->setCellValueByColumnAndRow(6, $sheet_row, $price);
+                $sheet->getActiveSheet()->getStyleByColumnAndRow(6, $sheet_row)->getNumberFormat()->setFormatCode($currency);
+                $sheet_row++;
+            }
+            $sheet_row++;
         }
+
+        // Export Excel file.
+        $writer = \PHPExcel_IOFactory::createWriter($sheet, 'Excel2007');
+        $writer->save('export.xlsx');
+
     }
 
 }
